10ab18fcb742ffa4a496393d86d1fe62abb78eb4
andrev36 <wiadernyandrzej96@gmail.com>
2020-08-26 21:48:57 +0200

    📦 NEW: Webpack configurations and TS

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,111 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
- entry: { app: './src/index.js' },
- output: {
-  filename: 'index.js',
-  path: path.resolve(__dirname, 'dist')
- },
- module: {
-  rules: [
-   // { test: /\.css$/, use: 'css-loader' },
-   { test: /\.ts$/, use: 'ts-loader' },
-   { test: /\.sss$/, loader: 'sass-loader' }
+const settings = {
+ distPath: path.join(__dirname, 'dist'),
+ srcPath: path.join(__dirname, 'src')
+};
+
+function srcPathExtend(subpath) {
+ return path.join(settings.srcPath, subpath);
+}
+
+module.exports = (options) => {
+ return {
+  devtool: 'source-map',
+  mode: 'development',
+  entry: path.join(__dirname, 'src', 'index.tsx'),
+  watch: true,
+  output: {
+   filename: 'index.js',
+   path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+   rules: [
+    // {
+    //  test: /\.js?$/,
+    //  query: {
+    //   presets: [
+    //    [
+    //     '@babel/env',
+    //     {
+    //      targets: {
+    //       browsers: 'last 2 chrome versions'
+    //      }
+    //     }
+    //    ]
+    //   ]
+    //  },
+    //  loader: 'babel-loader',
+    //  include: [path.resolve(__dirname, 'src')],
+    //  exclude: [path.resolve(__dirname, 'node_modules')]
+    // },
+    {
+     test: /\.tsx?$/,
+     use: ['babel-loader', 'ts-loader', 'tslint-loader']
+    },
+    {
+     test: /\.scss$/,
+     use: [
+      'style-loader',
+      {
+       loader: 'css-loader',
+       options: {
+        sourceMap: true
+       }
+      },
+      {
+       loader: 'postcss-loader',
+       options: {
+        plugins: [require('autoprefixer')()],
+        sourceMap: true
+       }
+      },
+      {
+       loader: 'sass-loader',
+       options: {
+        sourceMap: true
+       }
+      }
+     ]
+    },
+    {
+     test: /\.(ttf|eot|woff|woff2)$/,
+     use: {
+      loader: 'file-loader',
+      options: {
+       name: 'fonts/[name].[ext]'
+      }
+     }
+    },
+    {
+     test: /\.(jpe?g|png|gif|svg|ico)$/i,
+     use: [
+      {
+       loader: 'file-loader',
+       options: {
+        outputPath: 'assets/'
+       }
+      }
+     ]
+    }
+   ]
+  },
+  resolve: {
+   extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+  devServer: {
+   contentBase: './dist',
+   inline: true,
+   host: 'localhost',
+   port: 3000
+  },
+  plugins: [
+   new CleanWebpackPlugin({ verbose: true }),
+   new HtmlWebpackPlugin({
+    template: srcPathExtend('index.html')
+   })
   ]
- }
+ };
 };
