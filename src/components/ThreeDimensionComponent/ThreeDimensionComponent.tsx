45c9fb8e53851077375068130921d54508bf00bb
andrev36 <wiadernyandrzej96@gmail.com>
2020-11-04 20:06:57 +0100

    📦 NEW: Custom 3D model loaded and rendered

--- src/components/ThreeDimensionComponent/ThreeDimensionComponent.tsx
+++ src/components/ThreeDimensionComponent/ThreeDimensionComponent.tsx
@@ -3,22 +3,17 @@ import React, { Suspense } from 'react';
 import { Canvas } from 'react-three-fiber';
 
 const DuckModel = () => {
- // const gltf = useGLTF('./src/assets/models/Duck/glTF/Duck.gltf', true);
  const gltf = useGLTF('./models/Duck/glTF/Duck.gltf', true);
  return <primitive object={gltf.scene} dispose={null} />;
 };
 
-// * NOTE My StackOverflow question answered:
-//   https://stackoverflow.com/questions/64526248/webpack-in-react-cant-load-the-3d-model-with-a-gltf-extension-shows-404-not-fo/64564323#64564323
-// * NOTE Public path Webpack: https://webpack.js.org/guides/public-path/
 export const ThreeDimensionComponent = () => {
  return (
   <>
-   <Canvas camera={{ position: [0, 0, 10], fov: 70 }}>
+   <Canvas camera={{ position: [0, 0, 3], fov: 80 }}>
+    <ambientLight intensity={0.3} />
     <Suspense fallback={null}>
-     <mesh position={[0, 250, 0]}>
-      <DuckModel />
-     </mesh>
+     <DuckModel />
     </Suspense>
    </Canvas>
   </>
